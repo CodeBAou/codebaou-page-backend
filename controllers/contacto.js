caf101a84cf4d896e3cf09c1a4bc0ca3a9e7f517
codebaou <borisafuy@gmail.com>
2023-05-05 12:45:49 -0300

    se añade funcionalidad pedir cv

--- controllers/contacto.js
+++ controllers/contacto.js
@@ -56,7 +56,57 @@ const contactoPost = async (req=request, res=response) => {
     
 }
 
+
+const CV = async (req=require, res = response) => {
+
+    const body = req.body;
+    
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          type:'login',
+          user: process.env.emailnodemailer, // cuenta de gmail para envio
+          pass: process.env.passAplicationGoogleNodemailer, // contraseña de aplicacion creada desde la cuenta de google
+        },
+    });
+     
+    //DEFINIR DATA PARA EL ENVIO
+     //DATA PARA EL ENVIO
+    const data = {
+        from: `me <${process.env.emailnodemailer}>`, // sender address
+        to: process.env.emailnodemailer, // list of receivers
+        subject: "codebaoupage     -          NEW MENSAJE✔      de       " + body.emisor, // Subject line
+        text: body.mensaje , // plain "Hello world?"
+    };
+    
+    try{
+        //Envio
+        await transporter.sendMail(data, (err,info) =>{
+            if(err) res.status(500).json({
+                msg:"No se ha podido enviar el email",
+                err
+            });
+
+            if(info) res.status(200).json({
+                msg:"Se ha enviado el email!",
+                info
+            });
+        });
+
+    }catch(err){
+        res.status(500).json({
+            msg:'Se ha producido un error en el servidor',
+            err:err
+        });
+    }
+
+}
+
 module.exports={
-    contactoPost
+    contactoPost,
+    CV
 }
 
--- routes/contacto.routes.js
+++ routes/contacto.routes.js
@@ -1,8 +1,8 @@
 const {Router}      = require('express');
 const router        = Router();
-const {contactoPost} = require('../controllers/contacto');
+const {contactoPost,CV} = require('../controllers/contacto');
 
 //Aqui se guardar una coleccion de rutas para el apartado contacto
 router.post('/', contactoPost);
-
+router.post('/cv',CV);
 module.exports = router;
